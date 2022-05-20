d9084e3b07e3ac323cf42451a961237f2ff6261e
rafafloresok <rafafloresok@gmail.com>
2022-05-19 23:15:37 -0300

    se incorpora cartContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { cartContext } from "./context/cartContext";
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailcontainer';
@@ -7,18 +8,20 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<ItemListContainer/>} />
-          <Route path="/category/:id" element={<ItemListContainer/>} />
-          <Route path="/itemDetail/:id" element={<ItemDetailContainer/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/*" element={<Navigate to="/" replace/>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <cartContext.Provider value={[]}>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<ItemListContainer/>} />
+            <Route path="/category/:id" element={<ItemListContainer/>} />
+            <Route path="/itemDetail/:id" element={<ItemDetailContainer/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/*" element={<Navigate to="/" replace/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </cartContext.Provider>
   );
 }
 
