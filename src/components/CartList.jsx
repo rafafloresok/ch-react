423fc4cdb4680dff0e707738bc5c2dd6c060e58b
rafafloresok <rafafloresok@gmail.com>
2022-06-07 22:23:18 -0300

    se optimiza codigo

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -5,9 +5,10 @@ import './CartList.css';
 
 export default function CartList({sendOrderManage}) {
     const [customerData, setCustomerData] = useState({});
-    const [customerDataOk, setCustomerDataOk] = useState(false);
-    const [customerMailOk, setCustomerMailOk] = useState(false);
-    const [dataError, setDataError] = useState(false);
+    const [nameError, setNameError] = useState(false);
+    const [phoneError, setPhoneError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [email2Error, setEmail2Error] = useState(false);
     const {cartList, clearCart, totalPrice} = UseCartContext();
 
     function changeHandler(e) {
@@ -17,18 +18,14 @@ export default function CartList({sendOrderManage}) {
         });
     }
 
-    function customerDataVerify() {
-        if (customerData.name && customerData.phone && customerData.email && customerData.email2) {
-            setCustomerDataOk(true);
-            if (customerData.email === customerData.email2) {
-                setCustomerMailOk(true);
-            }
-        }
-        return (customerDataOk && customerMailOk)
-    }
-
     function dataManage() {
-        customerDataVerify() ? sendOrderManage(customerData) : setDataError(true) ;
+        setNameError(!customerData.name);
+        setPhoneError(!customerData.phone);
+        setEmailError(!customerData.email);
+        setEmail2Error(!(customerData.email2 === customerData.email));
+        if (customerData.name && customerData.phone && customerData.email && (customerData.email2 === customerData.email)) {
+            sendOrderManage(customerData);
+        }
     }
 
     return (
@@ -39,12 +36,19 @@ export default function CartList({sendOrderManage}) {
             <p>Ingrese sus datos para enviar el pedido:</p>
             <form action="">
                 <input name="name" onChange={(e) => changeHandler(e)} type="text" placeholder="Nombre" />
+                {nameError && <span>Debe ingresar un nombre</span>}
+                <br />
                 <input name="phone" onChange={(e) => changeHandler(e)} type="tel" placeholder="Teléfono" />
+                {phoneError && <span>Debe ingresar un teléfono</span>}
+                <br />
                 <input name="email" onChange={(e) => changeHandler(e)} type="email" placeholder="Correo eléctronico" />
+                {emailError && <span>Debe ingresar un correo electrónico</span>}
+                <br />
                 <input name="email2" onChange={(e) => changeHandler(e)} type="email" placeholder="Repita correo electrónico" />
+                {email2Error && <span>El correo electrónico no coincide</span>}
+                <br />
                 <textarea name="comment" onChange={(e) => changeHandler(e)} id="" cols="30" rows="10"></textarea>
             </form>
-            {dataError ? <p>Alguno de los datos ingresados es incorrecto</p> : <p></p>}
             <button onClick={clearCart}>Vaciar pedido</button>
             <button onClick={dataManage}>Enviar pedido</button>
         </div>
