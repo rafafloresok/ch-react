0db5bd337343ba09ed48bd43d1925b0d2aa31bb3
rafafloresok <rafafloresok@gmail.com>
2022-06-28 14:56:19 -0300

    se incorpora toggle nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,11 @@ import './Nav.css';
 export default function Nav() {
     const [categories,setCategories] = useState([]);
     const [loader,setLoader] = useState(true);
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleNav = () => {
+        setShowMenu(!showMenu)
+    }
 
     useEffect(() => {
         dbQueryCollection('categories', false, 'name', setCategories, setLoader);
@@ -15,10 +20,11 @@ export default function Nav() {
 
     return (
         <nav className="navBar">
-            <ul className="navBar__list">
+            <button className="navBar__toggleBtn" onClick={toggleNav}><i className="bi bi-list"></i></button>
+            <ul className="navBar__list" style={{display: showMenu && 'flex'}}>
                 {loader?
                     <li style={{color: 'white', margin: 'auto'}}>Cargando categorías...</li>:
-                    categories.map((category) => <NavItem key={category.id} category={category}/>)
+                    categories.map((category) => <NavItem key={category.id} category={category} toggleNav={toggleNav}/>)
                 }
             </ul>
         </nav>
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 import './NavItem.css';
 
-export default function NavItem({category}) {
+export default function NavItem({category, toggleNav}) {
 
     return (
         <Link to={`/category/${category.name}`} style={{ textDecoration: 'none' }}>
-            <li className="navBar__item">{category.name}</li>
+            <li className="navBar__item" onClick={toggleNav}>{category.name}</li>
         </Link>
     );
 }
