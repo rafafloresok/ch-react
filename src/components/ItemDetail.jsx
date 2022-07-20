7500ca95b409314dfe9379f7fb9a63eebf435ff3
rafafloresok <rafafloresok@gmail.com>
2022-07-19 23:08:47 -0300

    se optimiza codigo

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -21,11 +21,16 @@ export default function ItemDetail({item}) {
 
     return (
         <div className="itemDetail">
+            <div className="itemDetail__mask"></div>
             <img className="itemDetail__img" src={item.img} alt="" />
             <div className='itemDetail__info'>
                 <h3 className="itemDetail__title">{item.name}</h3>
+                <p className="itemDetail__price">${item.price}</p>
                 <p className="itemDetail__detail">{item.detail}</p>
-                {inputType === 'itemCount' ? <ItemCount item={item} currentStock={item.stock - qtyInCart} onAdd={onAdd}/> : <BuyButtons/>}
+                {inputType === 'itemCount'?
+                    <ItemCount item={item} currentStock={item.stock - qtyInCart} onAdd={onAdd}/>:
+                    <BuyButtons/>
+                }
             </div>
         </div>
     );
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
 import Item from "./Item";
 
 import './ItemList.css';
 
 export default function ItemList({items}) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect ((() => {
+        setCategories(items.map((item) => item.category).filter((item, i, items) => item !== items[i-1]))
+    }),[items])
+    
     return (
         <div className="itemList">
             {!items.length?
-                <h1 className="itemList__empty-list-message">Proximamente vas a poder disfrutar una nueva especialidad!</h1>:
-                items.map((item) => <Item key={item.id} item={item}/>)
+                <h1 className="itemList__empty-list-message">Proximamente vas a poder disfrutar esta nueva especialidad!</h1>:
+                <>
+                    {categories.map((item) => 
+                        <div className="subList">
+                            <div className="subList__title-container">
+                                <h2 className="subList__title">{item}</h2>
+                            </div>
+                            <div className="subList__items-container">
+                                {items.map(function(el) {return(el.category===item && <Item key={el.id} item={el}/>)})}
+                            </div>
+                        </div>
+                    )}
+                </>
             }
         </div>
     );
