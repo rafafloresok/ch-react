4a6de24e2fce7ee9fcd46718cf24dd99c225a988
rafafloresok <rafafloresok@gmail.com>
2022-08-29 23:02:04 -0300

    optimize form validation

--- src/components/CartForm.jsx
+++ src/components/CartForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { UseDbContext } from "../context/DbContext";
 import { UseCartContext } from "../context/CartContext";
+import { useFormValidation } from "../hooks/useFormValidation";
 
 import './CartForm.css';
 
@@ -9,12 +10,12 @@ export default function CartForm () {
     const {totalPrice, cartList, clearCart} = UseCartContext();
 
     const [customerData, setCustomerData] = useState({});
-    const [email2, setEmail2] = useState();
-    const [nameError, setNameError] = useState(false);
-    const [phoneError, setPhoneError] = useState(false);
-    const [emailError, setEmailError] = useState(false);
-    const [email2Error, setEmail2Error] = useState(false);
-    const [commentsError, setCommentsError] = useState(false);
+    const {name, phone, email, emailRepeat} = customerData;
+    const formFilled = (!!name && !!phone && !!email && !!emailRepeat)
+
+    const formErrors = useFormValidation(customerData);
+    const {nameErr, phoneErr, emailErr, emailRepeatErr, commentsErr} = formErrors;
+    const formSuccess = (!nameErr && !phoneErr && !emailErr && !emailRepeatErr && !commentsErr);
 
     const handleChange = (e) => {
         setCustomerData({
@@ -22,27 +23,9 @@ export default function CartForm () {
             [e.target.name]: e.target.value.trim()
         });
     }
-
+    
     const handleSubmit = () => {
-        const {name, phone, email, comments} = customerData;
-        let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-        let regexPhone = /^[0-9/-]+$/;
-        let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-        let regexComments = /^.{0,255}$/;
-
-        let nameErr = !name || !regexName.test(name);
-        let phoneErr = !phone || !regexPhone.test(phone);
-        let emailErr = !email || !regexEmail.test(email);
-        let email2Err = !(email2 === email);
-        let commentsErr = !regexComments.test(comments);
-
-        setNameError(nameErr);
-        setPhoneError(phoneErr);
-        setEmailError(emailErr);
-        setEmail2Error(email2Err);
-        setCommentsError(commentsErr);
-        
-        if (!nameErr && !phoneErr && !emailErr && !email2Err && !commentsErr) {
+        if (formFilled && formSuccess) {
             createOrder(customerData, totalPrice, cartList, clearCart);
         }
     }
@@ -53,17 +36,17 @@ export default function CartForm () {
                 <p className="cartForm__title">Ingrese sus datos:</p>
                 <form action="">
                     <input className="cartForm__input" name="name" onChange={(e) => handleChange(e)} type="text" placeholder="Nombre" />
-                    {nameError && <span className="cartForm__error">Debe ingresar un nombre válido</span>}
+                    {nameErr && <span className="cartForm__error">Debe ingresar un nombre válido</span>}
                     <input className="cartForm__input" name="phone" onChange={(e) => handleChange(e)} type="tel" placeholder="Teléfono" />
-                    {phoneError && <span className="cartForm__error">Debe ingresar un teléfono válido</span>}
+                    {phoneErr && <span className="cartForm__error">Debe ingresar un teléfono válido</span>}
                     <input className="cartForm__input" name="email" onChange={(e) => handleChange(e)} type="email" placeholder="Correo eléctronico" />
-                    {emailError && <span className="cartForm__error">Debe ingresar un correo electrónico válido</span>}
-                    <input className="cartForm__input" name="email2" onChange={(e) => setEmail2(e.target.value.trim())} type="email" placeholder="Repita correo electrónico" />
-                    {email2Error && <span className="cartForm__error">El correo electrónico no coincide</span>}
+                    {emailErr && <span className="cartForm__error">Debe ingresar un correo electrónico válido</span>}
+                    <input className="cartForm__input" name="emailRepeat" onChange={(e) => handleChange(e)} type="email" placeholder="Repita correo electrónico" />
+                    {emailRepeatErr && <span className="cartForm__error">El correo electrónico no coincide</span>}
                     <textarea className="cartForm__textarea" name="comments" onChange={(e) => handleChange(e)} id="" cols="30" rows="10" placeholder="Escriba aquí sus comentarios..." ></textarea>
-                    {commentsError && <span className="cartForm__error">Los comentarios no deben superar los 255 caracteres</span>}
+                    {commentsErr && <span className="cartForm__error">No debe superar los 255 caracteres</span>}
                 </form>
-                <button className="cartForm__button" onClick={handleSubmit}>Enviar pedido</button>
+                <button className="cartForm__button" onClick={handleSubmit} style={{opacity: formFilled || '0.5', cursor: formFilled ? 'pointer' : 'default'}} >Enviar pedido</button>
             </div>
         </>
     );
---
+++ src/hooks/useFormValidation.js
@@ -0,0 +1,38 @@
+export const useFormValidation = (data) => {
+    let errors = {};
+    let regEx;
+    let error;
+
+    for (const key in data) {
+        switch (key) {
+            case 'name':
+                regEx = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+                error = !data[key] || !regEx.test(data[key]);
+                break;
+            case 'phone':
+                regEx = /^[0-9/-]+$/;
+                error = !data[key] || !regEx.test(data[key]);
+                break;
+            case 'email':
+                regEx = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+                error = !data[key] || !regEx.test(data[key]);
+                break;
+            case 'emailRepeat':
+                error = !(data[key] === data.email);
+                break;
+            case 'comments':
+                regEx = /^.{0,255}$/;
+                error = !regEx.test(data[key]);
+                break;
+            default:
+                error = false;
+                break;
+        }
+        errors = {
+            ...errors,
+            [`${key}Err`]: error
+        }
+    }
+
+    return errors
+}
